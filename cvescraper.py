33364bc961c2b62a7a4f8e5580e10d7a0d2e2b06
Pearlesce <143547164+Pearlesce@users.noreply.github.com>
2024-03-12 22:24:56 -0400

    Add files via upload

--- cvescraper.py
+++ cvescraper.py
@@ -1,16 +1,28 @@
 import csv
 import requests
-import schedule
-import datetime
+from datetime import datetime
+import os
 
+'''
+SecureScape Vulnerability Scanner API
+Liz Aldem
+March 12, 2024
+'''
 
 def initialScrape():
-    url = 'https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv'
+    '''
+    This function is an API that scrapes the CSV information from the CISA Known Exploited
+    Vulnerabilities database and saves it into a CSV file
+    '''
+    url = "https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv"
 
     response = requests.get(url)
 
-    if response.status_code == 200:
+    '''
+    Test for a connection, throws an error if the program can't connect, and writes the csv file
+    '''
 
+    if response.status_code == 200:
 
         with open("data.csv", "wb") as f:
             f.write(response.content)
@@ -19,30 +31,48 @@ def initialScrape():
     else:
         print(f"Error: Failed to download file. Status code: {response.status_code}")
 
-def timedScraper():
-    url = 'https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv'
 
-    save_path = 'data.csv'
+def currentScrape(filename):
 
-    try:
-        response = requests.get(url)
+    '''
+    This function takes the saved CSV file from the initialScrape() function and
+    parses the data to only have data from the year 2024 in the CSV file
+    '''
 
-        if response.status_code == 200:
+    check_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
 
-            with open(save_path, "a", newline="") as f:
-                writer = csv.writer(f)
-                writer.writerow([datetime.datetime.now(), response.text])
-                print(f"File updated successfully at {datetime.datetime.now()}.")
+    with open(filename, "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
 
-        else:
-            print(f"Error: Failed to download file. Status code: {response.status_code}")
-    except:
-        print(f"An error occurred: {Exception}")
+        selected_data = []
 
+        for row in csv_reader:
+            row_date = datetime.strptime(row["dateAdded"], "%Y-%m-%d")
 
-'''For debugging'''
-schedule.every(3).minutes.do(timedScraper)
+            if row_date > check_date:
+                selected_data.append(row)
 
-'''For daily scheduled downloads'''
-'''schedule.every().day.at("12:00").do(timedScraper)'''
+    fieldnames = selected_data[0].keys()
 
+    with open("newdata.csv", "w", newline="") as new_csv:
+        writer = csv.DictWriter(new_csv, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for row in selected_data:
+            writer.writerow(row)
+        print("File Parsed!")
+
+def main():
+
+    file_exists = os.path.isfile("data.csv")
+
+    if not file_exists:
+        print("Running initialScrape")
+        initialScrape()
+    print("Parsing the data...")
+    currentScrape("data.csv")
+    os.remove("data.csv")
+
+if __name__ == "__main__":
+    main()
