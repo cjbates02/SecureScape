c085e6bac1ef0788f0a0af487e9296eb7a6a049e
Christian Bates <105766810+cjbates02@users.noreply.github.com>
2024-03-27 20:22:54 -0400

    syntax error

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def get_endpoints_data(password):
 
 
 
-if platform.system == 'Windows':
+if platform.system() == 'Windows':
     eel.browsers.set_path('electron', 'node_modules\electron\dist\electron.exe')
 else:
     eel.browsers.set_path('electron', 'node_modules/.bin/electron')
